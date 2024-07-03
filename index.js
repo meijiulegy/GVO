a9e3e6493790d5b85e0e787dd8ce4cc91512d5b1
Y Gu <cv2019_gy@163.com>
2024-07-03 11:33:21 +0200

    3-7

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let myStepCounter = 0;
 let blindSpotX = screen.availWidth/4;
+
 console.log('myStepCounter = ' + myStepCounter);
 
 localStorage.setItem('myStepCounter', JSON.stringify(myStepCounter));
